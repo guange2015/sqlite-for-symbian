7101dad3ec33adfa29b18ea50f38079e15a9f0af
hhuai <8863824@gmail.com>
2010-11-28 19:36:11 +0800

    successed for compiled by armcc and finish memory leak

---
+++ inc/sqlitelib.h
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name		: sqlitelib.h
+ Author	  : xiaoguang.huang
+ Copyright   : Your copyright notice
+ Description : Static lib header file
+ ============================================================================
+ */
+
+#ifndef __SQLITELIB_H__
+#define __SQLITELIB_H__
+
+//  Include Files
+
+#include <e32base.h>
+
+//  Function Prototypes
+
+TInt DummyFunction();
+
+#endif  // __SQLITELIB_H__
+
---
+++ src/sqlitelib.cpp
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name		: sqlitelib.cpp
+ Author	  : xiaoguang.huang
+ Copyright   : Your copyright notice
+ Description : Static lib source file
+ ============================================================================
+ */
+
+//  Include Files  
+
+#include "sqlitelib.h"
+
+//  Local Functions
+
+TInt DummyFunction()
+	{
+	//
+	// add your code here
+	//
+	return 0;
+	}
